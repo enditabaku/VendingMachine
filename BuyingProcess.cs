82a9f48364233337b01154c5c5d5154539b325f5
Endi Tabaku <endi.tabaku@hotmail.com>
2022-01-13 00:20:03 +0100

    Added Buying Class, Help Command Options and displayed all product details

---
+++ BuyingProcess.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachineConsoleApp
+{
+    class BuyingProcess
+    {
+        public double TotalAmount { set; get; }
+        public double LeftAmount { get; set; }
+        public double CurrentProductPrice { get; set; }
+        public BuyingProcess(double enteredAmount, double currentProductPrice = 0.00)
+        {
+            TotalAmount = enteredAmount;
+            LeftAmount = enteredAmount;
+            CurrentProductPrice = currentProductPrice;
+        }
+        public void ShowTotalAmountEntered()
+        {
+            Console.WriteLine("Amount entered: " + TotalAmount + " Euro");
+        }
+        public void ShowTotalAmountLeft()
+        {
+
+        }
+        public void AmountDeduction()
+        {
+            TotalAmount = TotalAmount - CurrentProductPrice;
+        }
+        public bool HasEnoughMoney()
+        {
+            if (LeftAmount > CurrentProductPrice)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
+
---
+++ UnpredictedInputException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VendingMachineConsoleApp
+{
+    class UnpredictedInputException : Exception
+    {
+        public UnpredictedInputException()
+        {
+            Console.WriteLine("Your input is not one of the predicted inputs. Please read the text above again and make sure you enter an accepted input");
+        }
+    }
+}
